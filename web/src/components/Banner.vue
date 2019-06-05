7403b086bdd08e14a65f101bd1d97fdfc30427eb
zekroTJA <riho@live.de>
2019-06-05 08:42:23 +0200

    fix style scopes

--- web/src/components/Banner.vue
+++ web/src/components/Banner.vue
@@ -28,7 +28,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 
 .banner {
   padding: 10px 15px;
--- web/src/components/Header.vue
+++ web/src/components/Header.vue
@@ -36,7 +36,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 
 .header > button {
   position: relative;
